069149c5b61959dd6f5c349e1f01e4b45beb6bcb
Avalynn Judge <avalynnjudge@gmail.com>
2022-04-10 09:45:11 -0700

    Added me query functionality

--- main/server/schemas/typeDefs.js
+++ main/server/schemas/typeDefs.js
@@ -33,7 +33,7 @@ const typeDefs = gql`
   }
   type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(username: Sting!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: BookInput): User
     removeBook(bookId: ID): User
   }
